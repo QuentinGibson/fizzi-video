470f1fd8a88bfd4ac5ae2765fdfd00ee95ab7329
Quentin Gibson <quentingibson94@gmail.com>
2024-09-08 10:05:49 -0400

    add logo

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FizziLogo } from "@/app/components/FizziLogo";
+
+// type Props = {};
+
+const Header = () => {
+  return (
+    <header className="flex justify-center py-4 -mb-28">
+      <FizziLogo className="z-10 h-20 cursor-pointer text-sky-800" />
+    </header>
+  );
+};
+
+export default Header;
